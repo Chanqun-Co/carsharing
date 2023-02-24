ee37f04f21619fdb8e98a416ff7a3543438fa2fb
NewEgoDocs <juhan211@naver.com>
2023-02-24 22:14:47 +0900

    carsharing-24 픽스처를 사용한 테스트 작성 및 User의 Equality 사용을 통한 비교 적용

--- core/src/test/kotlin/io/sharing/server/core/product/domain/ProductTest.kt
+++ core/src/test/kotlin/io/sharing/server/core/product/domain/ProductTest.kt
@@ -1,35 +1,30 @@
 package io.sharing.server.core.product.domain
 
-import io.sharing.server.core.user.domain.User
+import io.sharing.server.core.support.fixture.UserFixture
+import io.sharing.server.core.user.domain.Region
 import org.assertj.core.api.Assertions.*
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 
 class ProductTest {
 
     @Test
     fun `상품 생성`() {
-        val email = "woogie@gmail.com"
-        val firstName = "firstName"
-        val lastName = "lastName"
-        val birthDay = LocalDate.now()
-        val user = User.create(email, firstName, lastName, birthDay)
+        val user = UserFixture.get()
         val distance = 20000
-        val fee = 500
+        val rentalFee = 500
         val licensePlate = "사와1234"
         val productStatus = ProductStatus.CREATED
-        val location = Location.Tokyo
+        val region = Region.DANGSAN
         val description = "TestTest"
 
-        val product = Product(distance, fee, licensePlate, user, productStatus, location, description)
+        val product = Product(distance, rentalFee, licensePlate, user, productStatus, region, description)
 
         assertThat(product.distance).isEqualTo(distance)
-        assertThat(product.fee).isEqualTo(fee)
+        assertThat(product.rentalFee).isEqualTo(rentalFee)
         assertThat(product.licensePlate).isEqualTo(licensePlate)
-        assertThat(product.user).isNotNull
+        assertThat(product.user).isEqualTo(user)
         assertThat(product.productStatus).isEqualTo(ProductStatus.CREATED)
-        assertThat(product.location).isEqualTo(location)
+        assertThat(product.region).isEqualTo(region)
         assertThat(product.description).isEqualTo(description)
     }
 }
