fc7c3a7d76d09592c13811ca9b1ef0add122f485
KimSeHwan <ksh9241@naver.com>
2023-03-11 17:29:08 +0900

    carsharing-29 유저 상태변경 테스트

--- core/src/test/kotlin/io/sharing/server/core/user/application/port/inp/InactivateUserTest.kt
+++ core/src/test/kotlin/io/sharing/server/core/user/application/port/inp/InactivateUserTest.kt
@@ -2,12 +2,10 @@ package io.sharing.server.core.user.application.port.inp
 
 import io.sharing.server.core.support.test.BaseServiceTest
 import io.sharing.server.core.user.application.port.outp.UserRepository
-import io.sharing.server.core.user.domain.User
 import io.sharing.server.core.user.domain.UserStatus.*
 import io.sharing.server.core.user.domain.createUser
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 
 internal class InactivateUserTest(
     private val inactivateUser: InactivateUser,
@@ -16,9 +14,11 @@ internal class InactivateUserTest(
 
     @Test
     fun `유저 비활성화`() {
-        val user = userRepository.save(createUser())
-
+        val user = userRepository.save(createUser().apply {
+            this.status = ACTIVE
+        })
         val inactivateUserCommand = InactivateUserCommand(user.id!!)
+
         inactivateUser.inactivate(inactivateUserCommand)
 
         assertThat(user.status).isEqualTo(INACTIVE)
