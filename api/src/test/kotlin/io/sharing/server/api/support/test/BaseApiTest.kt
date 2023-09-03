2bf8226de345d8f2acf7bd34a1b362a287f52474
kimzerovirus <yg9699k@naver.com>
2023-09-03 21:37:06 +0900

    carsharing-65 mock 어노테이션 누락 수정

--- api/src/test/kotlin/io/sharing/server/api/support/test/BaseApiTest.kt
+++ api/src/test/kotlin/io/sharing/server/api/support/test/BaseApiTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
@@ -18,6 +19,7 @@ import org.springframework.web.context.WebApplicationContext
 @SpringBootTest
 @Transactional
 @ExtendWith(RestDocumentationExtension::class)
+@AutoConfigureMockMvc
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 abstract class BaseApiTest {
     @Autowired
